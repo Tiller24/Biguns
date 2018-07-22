9a211dae57afb2d1b14e5c9a937314d52bbcc657
Tyler Nolan <tylerjnolan24@gmail.com>
2018-07-22 10:41:38 -0600

    first commit

---
+++ BigUns.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import requests								#to get the website html
+import re									#regex to search for <p> tags starting with #
+import json
+import os
+from datetime import date					#to get the date
+from datetime import timedelta				#to do math on the date
+from bs4 	  import BeautifulSoup			#to parse the html
+
+from yt_id    import yt_id					#obtain the youtube id's of each video found
+
+from flask import Flask, render_template, request, jsonify
+application = Flask(__name__)
+
+urls   = []		#holds the pages to scrape for songs
+weeks  = []		#holds the title of the page being scraped
+titles = []		#the song titels of the videos
+max    = 0
+curr   = 2
+iframe = ""
+
+@application.route('/')
+def loading():
+	return render_template("loading.html")
+
+@application.route('/prev_biguns/', methods=['POST','GET'])
+def prev_biguns():
+	"""Create the index page, with latest biguns as the first video displayed"""
+	global iframe,urls,weeks,titles,max,curr
+	
+	urls   = [] #resets are for when index.html is refreshed
+	weeks  = [] #reset
+	titles = [] #reset
+	max    = 0  #reset
+	curr   = 2  #reset
+	iframe = "" #reset
+	
+	latest_biguns = first_page()
+	new_playlist(latest_biguns)
+	
+	return render_template("index.html",iframe = iframe,weeks = weeks,titles = titles)
+ 
+
+
+def first_page():
+	global max, urls, weeks
+	
+	all_biguns = 'http://hardrockdaddy.com/category/siriusxm-octane-big-uns-countdown/'		#domain
+	response = requests.get(all_biguns,headers={'User-Agent':'Mozilla/5.0'})				#request the webpage html
+	soup = BeautifulSoup(response.text,"html.parser")										#parse response
+	nums = soup.findAll("a", {"class" : "page-numbers"})									
+	max = int(nums[-2].getText())															#max page numbers to scrape
+	
+	blog_items = soup.findAll("div", {"class" : "home-blog-post-item"})						#find all the bigun links on first page
+	for b in blog_items:
+		urls.append(b.find("h2").find("a")['href'])											#add url to list of urls
+		weeks.append(b.find("h2").find("a").getText())										#the week names
+		
+	return urls[2] #starting from Week of 6/30/18; because they changed their html for the most recent week. Change index to match Week of 6/30/18!
+
+def new_playlist(url):
+	global iframe,titles
+	songlist  = scrape(url)
+	vid 	  = yt_id(songlist)
+	titles	  = vid.get_titles()
+	videos    = vid.get_videos()
+	iframe    = playlist(videos)
+	
+def scrape(website):
+	songlist = []	#store the songs and artist name
+	
+	response = requests.get(website,headers={'User-Agent':'Mozilla/5.0'})				#request the webpage html
+	soup = BeautifulSoup(response.text,"html.parser")									#parse response
+	songs = soup.findAll(text=re.compile("^#"))											#find the countdown of songs
+	
+	#the website had different formats for their html, 
+	#so I just made two ways to handle that for now
+	if len(songs) != 0:
+		for s in songs:
+			song = s[5:]
+			song = song.replace('–'," ")
+			songlist.append(song.replace(u'\xa0',u'') + str(s.next.text.strip() + " official music video"))			#store songs and artists
+			
+		print (songlist)
+		return songlist
+	else:
+		list = soup.findAll('ol')[0].text
+		songlist = list.strip().split("\n")
+		
+		return songlist
+	
+def playlist(videos):
+	iframe = videos[0] + '?playlist=' #open tag
+	for v in videos[1:]:
+		iframe += v + ','
+	iframe = iframe[:-1]		#remove trailing comma after the loop
+	return iframe	
+	
+	
+@application.route('/load_more/', methods = ['GET','POST'])
+def load_more():
+	global max, urls, weeks, curr
+	all_biguns = 'http://hardrockdaddy.com/category/siriusxm-octane-big-uns-countdown/'
+	size = len(weeks)
+	
+	if request.method == 'POST' and curr <= max:
+		
+		all_biguns += '/page/' + str(curr) + '/'
+		response = requests.get(all_biguns,headers={'User-Agent':'Mozilla/5.0'})		#request the webpage html
+		soup = BeautifulSoup(response.text,"html.parser")								#parse response
+		blog_items = soup.findAll("div", {"class" : "home-blog-post-item"})
+		for b in blog_items:
+			urls.append(b.find("h2").find("a")['href'])									#grabs the urls of the pages to parse
+			weeks.append(b.find("h2").find("a").getText())								#the names of the biguns
+		curr+=1
+		return jsonify(weeks[size:])
+	else:
+		return "none"
+
+@application.route('/new_request/',methods=['GET','POST'])		
+def new_request():
+	if request.method == 'POST':
+		index = int(request.form['index'])
+		url = urls[index] 																#grab desired index
+		new_playlist(url)																#url of the desired biguns week
+		iframe_src = 'https://www.youtube.com/embed/' + iframe
+		json_arr = json.dumps(titles)
+		
+		return jsonify('{"iframe" : "%s", "titles" : %s}' % (iframe_src,json_arr))
+
+if __name__ == "__main__":
+	ON_HEROKU = os.environ.get('ON_HEROKU')						#see if Heroku is running app
+	if ON_HEROKU:
+		application.run()
+	else:
+		port = 80
+		application.run(debug = True, port = port, host = '0.0.0.0')
+	
---
+++ README.txt
@@ -0,0 +1,104 @@
+Biguns Project is a project that finds the top 15 songs of the week 
+from Serius XM's Octane (channel 37)
+
+In the future I will change it to store the song IDs and other info
+in a database. This is because they are too inconsistent with
+the way they format their HTML. Plus, I'll be able to query
+much more quickly, and edit videos that are not accurate.
+
+So far, there really isn't much videos that are inaccurate
+
+ 
+
+Here is how each file works:
+
+=============================
+loading.html
+
+This is the template pulled up by loading() in Biguns.py
+
+The only function of this page is to show a loading gif while /prev_biguns/
+in Biguns.py is loading the first page of Bigun countdowns.
+
+=============================
+Biguns.py
+
+When the app loads up, Biguns.py renders the template 'loading.html',
+which just pulls up a cute loading screen.
+
+prev_biguns() 
+			  holds all the urls, name of the countdown (weeks),
+			  title of the videos, and max number of pages to scrape.
+			  
+			  first_page() grabs the front page of
+			  http://hardrockdaddy.com/category/siriusxm-octane-big-uns-countdown/
+			  That page contains the current countdown and the previous few weeks.
+			  Returns the latest countdown.
+			  
+			  new_playlist() uses the first countdown obtained from first_page()
+			  and scrapes it for the song titles. It stores the titles and resulting
+			  iframe
+			  
+
+first_page()  
+			  grabs the front page of
+			  http://hardrockdaddy.com/category/siriusxm-octane-big-uns-countdown/
+			  BeautifulSoup is used to scrape the webpage for the countdown.
+			  
+			  max is used to store the amount of pages we will scrape in the future.
+			  
+			  Returns the most recent countdown songs, but I currently set it to
+			  return only from Week of 6/30/18 and earlier because they changed
+			  the HTML of the weeks after that. CHANGE return urls[i] TO MATCH THAT Week
+			  IF YOU ARE TESTING.
+			  
+ 
+ new_playlist() 
+			  Accepts the url of the particular Countdown you are creating
+			  a playlist for, and Scrapes that url for the countdown. 
+
+			  Passes the songlist to yt_id.py, which queries using the YouTube API
+			  to find the video IDs and video Titles.
+			  
+			  Once it has that, playlist() is called to put the video IDs into
+			  a iframe.
+			  
+scrape()	
+			  Scapes the url for the song list using BeautifulSoup.
+			  There are two ways to scrape because they don't have consistent
+			  html layout. Which is why I will eventually put scraped videos
+			  into a database and query from there instead
+
+load_more()   
+			  This method is called when the 'Load more' button is pressed
+			  from the home page. Loads the page number indicated by 'curr' variable
+			  as long as curr < max #of pages.
+			  
+new_request()
+			  When a 'Week of' title is pressed on the right of the screen,
+			  this method grabs the index of that item and finds the url for 
+			  that week, and creates the playlist
+
+=============================			  
+yt_id.py
+
+This is the program that handles the Youtube API
+
+__init__ constructor()
+			  The constructor is given the songlist scraped from the website.
+			  It also has the video IDs, and the video Titles
+			  
+youtube_search()
+			  searches youtube by querying the songlist titles.
+			  Once it searches, it grabs the top result, and gets
+			  its ID and Title and stores them
+
+=============================
+index.html
+
+The load more button just shows more of the previous bigun weeks. 
+Clicking on one of those weeks loads the videos for that week.
+
+=============================
+
+
